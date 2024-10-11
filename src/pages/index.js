2af1fc5600bb1e06dfb2b6a7d2b1605d9214bc86
shahrul <shahrul.zulkifli@mdec.com.my>
2024-10-11 21:46:26 +0800

    add axios, update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000/api/issues";
+
 export default function Home() {
-  return <div>Sitemate assessment</div>;
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    fetchIssues();
+  }, []);
+
+  const fetchIssues = async () => {
+    try {
+      const response = await axios.get(BASE_URL);
+      setIssues(response.data);
+    } catch (error) {
+      console.error("Error fetching issues:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Issues</h1>
+      <ul>
+        {issues.map((issue) => (
+          <li key={issue.id}>
+            <h2>{issue.title}</h2>
+            <p>{issue.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
